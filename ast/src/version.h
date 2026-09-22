083e7ddc13b0b3f9d82e3af220c9ab7d161ea3ed
Tim Jenness <tjenness@lsst.org>
2026-09-22 12:38:29 -0700

    Sync with C AST release v9.5.0

--- ast/src/version.h
+++ ast/src/version.h
@@ -60,14 +60,14 @@
 *-
 */
 
-/* The current version of AST is 9.4.0 */
+/* The current version of AST is 9.5.0 */
 #define AST__VMAJOR    9
-#define AST__VMINOR    4
+#define AST__VMINOR    5
 #define AST__RELEASE   0
 
 /* Deprecated macros */
 #define AST_MAJOR_VERS 9
-#define AST_MINOR_VERS 4
+#define AST_MINOR_VERS 5
 #define AST_RELEASE    0
 
 #endif /* #if ! defined(VERSION_INCLUDED) */
